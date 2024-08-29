685f0197ebfa0f31c918717b02ec1bd81a9965d3
matheushodecker <matheus.hodecker.santos@gmail.com>
2024-08-28 21:04:48 -0300

    router arrumado

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/loja">loja</RouterLink>
         <RouterLink to="/cadastro">Cadastro</RouterLink>
         <RouterLink to="/vendas">Vendas</RouterLink>
       </nav>
